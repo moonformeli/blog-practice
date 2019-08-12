ccdf50703d0b34f8e4a9d2fd390ed5d20aebb73d
Moon <yangmungyu@yangmungyuui-MacBookPro.local>
2019-08-12 09:45:03 +0900

    mariadb is installed

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,86 @@
 import debug from "debug";
 import express, { Application, Request, Response } from "express";
+import MariaDB from "mariadb";
 
-const log = debug("Express");
+const log = debug("Express: app");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 const app: Application = express();
+
+const maria = MariaDB.createPool({
+  connectionLimit: 1,
+  host: "localhost",
+  password: "12345",
+  user: "root"
+});
+
+let conn: MariaDB.PoolConnection;
+(async () => {
+  try {
+    conn = await maria.getConnection();
+    await conn.query("use test");
+  } catch (e) {
+    console.error(e);
+  }
+})();
 
 app.get("/", (req: Request, res: Response) => {
   log("Home: Get");
   res.send("hello");
+});
+
+app.get("/insert/:no/:title", async (req: Request, res: Response) => {
+  const { params } = req;
+  const { no, title } = params;
+
+  try {
+    await conn.query("insert into ttable value (?, ?)", [no, title]);
+    res.send("Item is successfully stored");
+    return res.end() as any;
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.get("/lists", async (req: Request, res: Response) => {
+  log("Get lists");
+
+  try {
+    if (!conn) {
+      conn = await maria.getConnection();
+      await conn.query("use test");
+    }
+
+    const lists = await conn.query("select * from ttable");
+    return res.send(lists);
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.get("/delete", async (req: Request, res: Response) => {
+  log("Delete all items");
+
+  try {
+    await conn.query("delete from ttable");
+    return res.send("All items are deleted.");
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+app.get("/update/:no/:title", async (req: Request, res: Response) => {
+  log("Update the item");
+
+  const { params } = req;
+  const { no, title } = params;
+
+  try {
+    await conn.query(`update ttable set title='${title}' where no=${no}`);
+    return res.send("The item is updated");
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 app.listen(PORT, () => {
